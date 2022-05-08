d77929b4ec1321a133055e47e9fa15adc4e3e1cb
Jonathan Shreckengost <jonathanshrek@gmail.com>
2022-05-08 15:13:26 -0400

    init commit

---
+++ .ccls-cache/@home@triz3n@Projects@cppRepo@Intro-Binary-Patching-Example/src@main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char *argv[]) {
+    // check to see if user provided 1 argument
+    if (argc != 2) {
+        std::cout << "You must provide exactly one argument." << std::endl;
+
+        return EXIT_FAILURE;
+    }
+
+    if (strcmp(argv[1],"AAAA-1234-5678-9012") == 0)
+    {
+        std::cout << "Access granted" << std::endl;
+    }
+    else
+    {
+        std::cout << "Access denied" << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Intro-Binary-Patching-Example
+### Intro to Binary Patching
+This is the example file used in my introduction to binary patching blog post at my [website](JonathanShrek.github.io).
